18df08b529928230af5017478cf70e1829fa2ed0
castro12321 <norbertkawinski@gmail.com>
2014-06-11 17:03:11 +0200

    Permissions adjustments

--- src/castro/connector/CWE.java
+++ src/castro/connector/CWE.java
@@ -47,20 +47,17 @@ public class CWE
 		check("aliquam.designer"    , 1000*k);
 		check("aliquam.architect"   , 1000*k);
 		
-		check("aliquam.welimit.25k" , 25*k);
 		check("aliquam.welimit.100k", 100*k);
-		check("aliquam.welimit.250k", 250*k);
 		check("aliquam.welimit.500k", 500*k);
-		check("aliquam.welimit.1m"  , 1*m);
 		check("aliquam.welimit.2m"  , 2*m);
 		check("aliquam.welimit.5m"  , 5*m);
 		
 		check("aliquam.welimit.x2"  , limit * 2);
 		check("aliquam.welimit.x22" , limit * 2);
-		check("aliquam.welimit.x222", limit * 2);
+		check("aliquam.welimit.x3"  , limit * 3);
+		check("aliquam.welimit.x33" , limit * 3);
 		check("aliquam.welimit.x5"  , limit * 5);
 		check("aliquam.welimit.x55" , limit * 5);
-		check("aliquam.welimit.x555", limit * 5);
 		
 		check("aliquam.mod", -1);
 		
